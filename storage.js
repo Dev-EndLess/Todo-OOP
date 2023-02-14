a6fa7c3bb9f934161597af8696773dfc3a206f83
Dev-EndLess <endlesspain1988@gmail.com>
2023-02-14 14:35:12 +0100

    localStorage Class added

---
+++ storage.js
@@ -0,0 +1,16 @@
+// Local Storage
+class Storage {
+  static addToStorage(todoArr) {
+    let storage = localStorage.setItem("todo", JSON.stringify(todoArr))
+    return storage
+  }
+
+  static getStorage() {
+    let storage = localStorage.getItem("todo") === null
+      ? []
+      : JSON.parse(localStorage.getItem("todo"))
+      return storage
+  }
+}
+
+export default Storage
